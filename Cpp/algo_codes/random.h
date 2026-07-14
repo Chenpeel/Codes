07a70f2636566fbfab13a5c8359a35dfe6628d64
Chenpeel <92452544+Chenpeel@users.noreply.github.com>
2026-07-14 19:09:51 +0800

    style(cpp): tidy random helper header

--- Cpp/algo_codes/random.h
+++ Cpp/algo_codes/random.h
@@ -1,9 +1,10 @@
+
 #include <random>
 using namespace std;
 
 // 生成随机数返回
 random_device rd;
-mt19937 gen(rd()); //梅森旋转算法的伪随机数
+mt19937 gen(rd()); // 梅森旋转算法的伪随机数
 uniform_int_distribution<> dis_int(1, 100);
 uniform_real_distribution<> dis_float(0.0, 1.0);
 
